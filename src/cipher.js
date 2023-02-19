2c1e184637ad6f533c5094b100f67d08fdea851d
FrancisCG97 <f.carrasco.g.97@gmail.com>
2023-02-19 13:05:19 -0300

    capturamos texto, y lo pasamos a ascii

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
+
 };
+//LOGICA
+
+const btnCifrar = document.getElementById('cifrar')
+
+const ascii = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+    85, 86, 87, 88, 89, 90]
+
+let index = 0;
+
+function prueba() {
+    let codigo = ascii.charCodeAt(index);
+    console.log(codigo);
+}
+
+function escuchar3() {
+
+    console.log("qwerty")
+    // const textoInicial = document.getElementById("txt-cifrar").value;
+    // console.log(textoInicial)
+    // document.getElementById("txt").innerHTML = textoInicial;
+}
+
+//  btnCifrar.addEventListener('click', escuchar)
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,83 @@
 import cipher from './cipher.js';
 
+//Elementos del DOM
+
+
+const borrar = document.getElementById('volver');
+const btnDescifrar = document.getElementById('descifrar')
+const btnCifrar = document.getElementById('cifrar')
+
+
+//FUNCIONES
+
+
+function escuchar() {
+  console.log("cifrar")
+
+  const textoInicial = document.getElementById("txt-cifrar").value;//capture el texto
+  console.log(textoInicial)//lo mostre en consola
+  document.getElementById("txt").innerHTML = textoInicial;//lo mostre abajo
+
+  //guardo el texto en un array
+  const textoInicialPartido = textoInicial.split('')//use split para dividir el texto 
+  console.log(textoInicialPartido)//lo mostre en consola
+  let largoArray = textoInicialPartido.length;
+  console.log(largoArray)
+
+  
+  
+for (let i = 0; i < largoArray; i++) {
+  console.log(
+    textoInicialPartido[i].charCodeAt(0));
+}
+
+
+
+
+
+
+  
+  // const textoChar = textoInicial.charCodeAt()
+  // console.log(textoChar)
+  
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function escuchar2() {
+
+  console.log("decifrar")
+  const textoInicial2 = document.getElementById("txt-descifrar").value;
+  console.log(textoInicial2)
+  document.getElementById("txt").innerHTML = textoInicial2;
+}
+
+// function remove() {
+//   console.log("funciona")
+// return borrar.parentNode.removeChild("txt");
+  
+// }
+
+btnDescifrar.addEventListener('click', escuchar2)
+btnCifrar.addEventListener('click', escuchar)
+// borrar.addEventListener('click', remove)
+
+
 console.log(cipher);
