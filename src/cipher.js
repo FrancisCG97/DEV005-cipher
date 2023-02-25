3e26f69e05dcd95b476581992cf8683065d5e5d7
FRANCISCG97 <f.carrasco.g.97@gmail.com>
2023-02-24 22:15:13 -0300

    Modificación función encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,31 @@
 const cipher = {
 
-    myMethod: function(params) {
-        // ...hacer algo
-      },
-      myMethod: function(params) {
+  encode: function (textoInicial, desplazamiento) {
+
+    const desplazamientoNumero = parseInt(desplazamiento);//document.getElementById("offset").value;
+    console.log(desplazamientoNumero);
+    let textoFinal = "";
+
+    for (let i = 0; i < textoInicial.length; i++) {
+      const charOriginal = textoInicial.charCodeAt(i); //textoInicialPartido[i].charCodeAt(0);
+      const textoAconvertir = charOriginal + desplazamientoNumero;
+
+      const letraConvertida = String.fromCharCode(textoAconvertir);
+      console.log(letraConvertida);
+
+      textoFinal += letraConvertida;
+
+      document.getElementById("txt").innerHTML = textoFinal;
+
+    }
+  },
+
+
+  myMethod: function (params) {
     // ...hacer algo
   }
 
 };
-//LOGICA
 
-//const btnCifrar = document.getElementById('cifrar')
-
-const ascii = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
-    85, 86, 87, 88, 89, 90]
-
-let index = 0;
-
-function prueba() {
-    let codigo = ascii.charCodeAt(index);
-    console.log(codigo);
-}
-
-function escuchar3() {
-
-    console.log("qwerty")
-    // const textoInicial = document.getElementById("txt-cifrar").value;
-    // console.log(textoInicial)
-    // document.getElementById("txt").innerHTML = textoInicial;
-}
-
-//  btnCifrar.addEventListener('click', escuchar)
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,115 +2,27 @@ import cipher from './cipher.js';
 
 //Elementos del DOM
 
+const btnDescifrar = document.getElementById('descifrar');
+const btnCifrar = document.getElementById('cifrar');
 
-// const borrar = document.getElementById('volver');
-const btnDescifrar = document.getElementById('descifrar')
-const btnCifrar = document.getElementById('cifrar')
-
-
+//EVENTOS
+btnCifrar.addEventListener('click', cifrar);
 
 //FUNCIONES
 
-//CIFRADO
-
-function escuchar() {
-  console.log("cifrar")
-
-  const desplazamiento = document.getElementById("offset").value;
-  console.log(desplazamiento)
-
-  const textoInicial = document.getElementById("txt-cifrar").value;//capture el texto
-  console.log(textoInicial)//lo mostre en consola
-  document.getElementById("txt").innerHTML = textoInicial;//lo mostre abajo
-
-
-  //guardo el texto en un array
-  const textoInicialPartido = textoInicial.split('')//use split para dividir el texto 
-  console.log(textoInicialPartido)//lo mostre en consola
-  let largoArray = textoInicialPartido.length;
-  console.log(largoArray)
-
-  //TEXTO A CODIGO ASCII
-
-  for (let i = 0; i < largoArray; i++) {
-    const charOriginal = textoInicialPartido[i].charCodeAt(0);
-    // console.log("TEXTO ORIGINAL " +charOriginal)
-    const textoAconvertir = charOriginal + + desplazamiento;
-    // console.log("TEXTO CONVERTIDO " + textoAconvertir)
-
-    const textoFinal = textoAconvertir.toString();
-    console.log(textoFinal)
-
-
-    const textoFinal2 = String.fromCharCode(textoFinal);
-    console.log(textoFinal2)
-
-    const letra = [textoFinal2]
-    console.log(letra)
-
-
-    let textoFinal3 = letra.toString();
-    console.log(textoFinal3)
-    // alert(textoFinal2)
-
-    document.getElementById("txt").innerHTML = textoFinal3;
-  }
-
-}
-
-
-
-  //DESCIFRADO
-
-  function escuchar_descifrado() {
-    console.log("descifrar")
+function cifrar() {
+    console.log("cifrar");
 
     const desplazamiento = document.getElementById("offset").value;
-    console.log(desplazamiento)
+    console.log(desplazamiento);
 
-    const textoInicialDescifrado = document.getElementById("txt-descifrar").value;//capture el texto
-    console.log(textoInicialDescifrado)//lo mostre en consola
-    document.getElementById("txt").innerHTML = textoInicialDescifrado;//lo mostre abajo
+    const textoInicial = document.getElementById("txt-cifrar").value;//capture el texto
+    console.log(textoInicial)//lo mostre en consola
+    document.getElementById("txt").innerHTML = textoInicial;//lo mostre abajo
 
-
-    //guardo el texto en un array
-    const textoInicialPartido_descifrado = textoInicialDescifrado.split('')//use split para dividir el texto 
-    console.log(textoInicialPartido_descifrado)//lo mostre en consola
-    let largoArray_descifrado = textoInicialPartido_descifrado.length;
-    console.log(largoArray_descifrado)
-
-    //CODIGO ASCII A TEXTO
-
-    for (let i = 0; i < largoArray_descifrado; i++) {
-      const charOriginal_descifrado = String.fromCharCode(textoInicialPartido_descifrado[i])//.toString();
-      //console.log("TEXTO ORIGINAL " +charOriginal_descifrado)
-      const textoAdescifrar = charOriginal_descifrado - - desplazamiento;
-      //console.log("TEXTO CONVERTIDO " + textoAconvertir)
-
-      const textoFinal_descifrado = Number.parseInt(textoAdescifrar);//.toString();
-      console.log(textoFinal_descifrado.toString())
-
-
-      const textoFinal_descifrado2 = String.fromCharCode(textoFinal_descifrado);//.toString();
-      console.log(textoFinal_descifrado2)
-
-      const codigoAscii = [textoFinal_descifrado2]
-      console.log(codigoAscii)
-
-
-      let textoFinal_descifrado3 = parseInt(codigoAscii.toString());
-      console.log(textoFinal_descifrado3)
-      // alert(textoFinal2)
-
-      document.getElementById("txt").innerHTML = textoFinal_descifrado3;
-
-
-    }
-
-  }
-
-  btnDescifrar.addEventListener('click', escuchar_descifrado)
-  btnCifrar.addEventListener('click', escuchar)
+    //LLAMAR FUNCION CIPHER
+    cipher.encode(textoInicial, desplazamiento);
+}
 
 
 
